b740ae99268e3c8bac4853988968a0398ac99f0c
starter <Starternh@gmail.com>
2021-01-18 03:00:27 +0300

    added new dependencies added few helpers

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Reva2\JsonApiBundle\Reva2JsonApiBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
---
+++ src/Controller/PostsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Utils\JsonApi\JsonApiErrorsTrait;
+use Exception;
+use Psr\Log\LoggerInterface;
+use Reva2\JsonApi\Annotations\ApiRequest;
+use Reva2\JsonApi\Contracts\Services\JsonApiServiceInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * Controller that implements posts API
+ *
+ * @author Konstantin Laktionov <konstantin.laktionov@orbitsoft.com>
+ * @package App\Controller
+ */
+class PostsController extends JsonApiController
+{
+    use JsonApiErrorsTrait;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var Security
+     */
+    protected $security;
+
+    /**
+     * BonusGroupsController constructor
+     *
+     * @param JsonApiServiceInterface $jsonApiService
+     * @param LoggerInterface $logger
+     * @param Security $security
+     */
+    public function __construct(
+        JsonApiServiceInterface $jsonApiService,
+        LoggerInterface $logger,
+        Security $security
+    ) {
+        parent::__construct($jsonApiService);
+
+        $this->logger = $logger;
+        $this->security = $security;
+    }
+
+    /**
+     * Create post
+     *
+     * @param Request $request
+     *
+     * @Route("/v1/posts", methods={"POST"}, name="posts.create")
+     * @ApiRequest(
+     *     body="App\Dto\PostDocument",
+     *     serialization={"Default", "CreatePost"}, validation={"Default", "CreatePost"}
+     * )
+     * @return Response
+     * @throws Exception
+     */
+    public function create(Request $request)
+    {
+        $apiRequest = $this->jsonApiService->parseRequest($request);
+        /* @var $post Post */
+        $post = $apiRequest->getBody()->data;
+
+        $this->service->create($post);
+
+        // return $this->buildEmptyResponse($apiRequest, Response::HTTP_NO_CONTENT);
+    }
+}
